52e5076501be2ceeafecce549521962b1b3180b0
Harini V <hariniv.cse2023@citchennai.net>
2026-07-20 17:08:41 +0530

    Connected frontend to deployed backend

--- backend/db.js
+++ backend/db.js
@@ -2,16 +2,20 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true
+    }
 });
 
 db.connect((err) => {
     if (err) {
         console.log("Database Error:", err);
     } else {
-        console.log("MySQL Connected");
+        console.log("TiDB Connected Successfully");
     }
 });
 
